77bcc4f6a7b8d7680808483f8b006decc5a82a89
fnaith <weichih.fr.lai@gmail.com>
2025-03-25 21:33:29 +0800

    apply gradle build

--- src/main/java/vn/com/orismaster/Example.java
+++ src/main/java/vn/com/orismaster/Example.java
@@ -119,8 +119,8 @@ public class Example {
     }
 
     public static void main(String... args) {
-        generateAndSaveToFile();
-//        generate();
+//        generateAndSaveToFile();
+        generate();
 //        solve();
     }
 }
